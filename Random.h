5bccfb8ede8dcb6bfcc1ac52a95a185bcdc19ee0
NanoCode012 <kevinvong@rocketmail.com>
2018-03-15 20:23:48 +0700

    Added Random header file

---
+++ Random.h
@@ -0,0 +1,28 @@
+#pragma once
+#include <stdlib.h>
+#include <time.h>
+
+bool hasSetSeed = false;
+
+int SetSeed(){
+    srand(time(NULL));
+    return 0;
+}
+
+int GetRandomNumber(){
+    if (!hasSetSeed) {
+        SetSeed();
+        hasSetSeed = true;
+    }
+    return rand();
+}
+
+int GetRandomNumber(int max){
+    if (max <= 0) return -1;
+    else return GetRandomNumber() % (max + 1);
+}
+
+int GetRandomNumber(int min, int max){
+    if (min < 0 || max <= 0) return -1;
+    else return min + GetRandomNumber(max - min);
+}
