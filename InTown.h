589a7d751317c0ad72ac1ad733c8f9c49fd8f8d1
NanoCode012 <kevinvong@rocketmail.com>
2018-03-15 20:25:08 +0700

    Added In-Town, and Inn

---
+++ InTown.h
@@ -0,0 +1,89 @@
+#pragma once
+#include <iostream>
+#include "./Characters.h"
+#include "./Random.h"
+
+using namespace std;
+
+void OptionsInTown()
+{
+    cout << "I looked around ... "           << endl;
+    cout << "What would you like to do?"     << endl
+         << "Opt : "                         << endl
+         << "1. Visit Inn"                   << endl
+         << "2. Check Misionboard"           << endl
+         << "3. Go to Item shop"             << endl
+         << "4. Look at stats and inventory" << endl
+         << "5. Leave village"               << endl;
+}
+
+void Inn(Character &player, bool isDead = false)
+{
+    if (!isDead)
+    {
+        char opt;
+        cout << "\"Welcome to the Inn."                                         << endl
+             << "Would you like to stay here to relax and rejuvenate for 5G?" << endl;
+        cout << "Opt (Y/N) : ";
+        cin >> opt;
+        if (opt == 'Y' || opt == 'y') 
+        {
+            if (player.gold >= 5)
+            {
+                player.Fullheal();
+                player.gold -= 5;
+                cout << "Thank you for resting here!" << endl;
+            }
+            else
+            {
+                cout << "Sorry you don't have enough G." << endl;
+            }
+
+        }
+    }
+    else
+    {
+        cout << "\"Hello ? .... Hello ? Are you OK? I found you fainted in the wild "
+             << "and brought you back here. It seems like you're getting better now.\""
+             << ", said the Inn Owner."
+             << endl;
+        int cost = GetRandomNumber(5, 15);
+        if (player.gold >= cost) 
+        {
+            cout << "\"By the way, it cost me quite a bit to bring you here, so I'm taking "
+                 << "a bit of money from you.\", pointed the Owner."
+                 << endl;
+        }
+        else 
+        {
+            cout << "\"By the way, it cost me quite a bit to bring you here, so I'm taking "
+                 << "a bit of money from you, but you don't have enough. You owe me "
+                 << cost - player.gold
+                 << "G\", sneakily pointed the Owner."
+                 << endl;
+        }
+        player.gold -= cost;
+    }
+}
+
+void InTown(Character &player)
+{
+    int opt;
+    if (player.currentHp == 0) Inn(player, true);
+    while(true)
+    {
+        OptionsInTown();
+        cin >> opt;
+        if (opt == 5) break;
+        switch(opt)
+        {
+            case 1:
+                Inn(player);
+                break;
+            case 2:
+                // MissionBoard(player);
+                break;
+        }
+    }
+}
+
