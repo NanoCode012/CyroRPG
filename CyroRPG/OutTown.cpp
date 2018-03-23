01f2990f066b7ee9469aea23fb249654a55b9222
NanoCode012 <kevinvong@rocketmail.com>
2018-03-23 14:03:02 +0700

    Added battle scene. Added finished monster attack calculation.

--- CyroRPG/OutTown.cpp
+++ CyroRPG/OutTown.cpp
@@ -23,7 +23,7 @@ void Options(int playerPosition)
     
 }
 
-void Calculate(Character &player, int opt, bool &exitStatus)
+void MoveCharacter(Character &player, int opt, bool &exitStatus)
 {
     switch(opt)
         {
@@ -42,18 +42,174 @@ void Calculate(Character &player, int opt, bool &exitStatus)
         }
 }
 
+bool CheckIfMeetEnemy(Character &player)
+{
+    int num = GetRandomNumber(100);
+    if (player.position != 0 && player.position % 5 == 0)
+    {
+        return true;
+    }
+    return (num <= 35 + player.position * 4);
+}
+
+void SetPlayerStats(Character &player, Monster &monster)
+{
+    player.SetTempStatsEqualToNonTemp();
+    for (int i = 0; i < player.amountOfSkills; i++)
+    {
+        if (!player.skills[i].isActive) 
+        {
+            player.UseSkill(i, monster);
+        }
+    }
+}
+
+void SetEnemyStats(Monster &monster, Character &player)
+{
+    monster.SetType((player.position / 5) + 1, player.position);
+    monster.SetTempStatsEqualToNonTemp();
+}
+
+void ShowBattleOptions()
+{
+    PrintLine();
+    cout << "Your turn .."      << endl;
+    cout << "Opt : "            << endl
+         << "1. Normal Attack"  << endl
+         << "2. Use Skill"      << endl
+         << "3. Use Item"       << endl;
+}
+
+void CalculatePlayerAttack(Character &player, Monster &monster, int opt)
+{
+    int rand = GetRandomNumber(100);
+    switch(opt)
+    {
+        case 1:
+            if (rand >= 100 - player.chanceOfCritical)
+            {
+                monster.currentHp -= player.CriticalAttack();
+            }
+            else
+            {
+                monster.currentHp -= player.NormalAttack();
+            }
+            break;
+    }
+}
+
+void CalculateMonsterAttack(Monster &monster, Character &player, int opt)
+{
+    int rand = GetRandomNumber(100);
+    float damage;
+    //Type of attack
+    if (rand >= 100 - (monster.tempChanceOfSpecialAttack * 100)) 
+    {
+        cout << "Enemy used Special Attack" << endl;
+        damage = monster.SpecialAttack();
+    }
+    else if (rand >= 100 - (monster.tempChanceOfSpecialAttack*100) - (monster.tempChanceOfCritical*100)) 
+    {
+        cout << "Enemy does Critical Attack" << endl;
+        damage = monster.CriticalAttack();
+    }
+    else 
+    {
+        cout << "Enemy does Normal Attack" << endl;
+        damage = monster.NormalAttack();
+    }
+
+    //Half of defense is how much damage is removed
+    if ((player.defense / 2) >= damage)
+    {
+        cout << "Your defense negated the enemy's attack" << endl;
+    }
+    else 
+    {
+        damage -= player.defense / 2;
+        
+        //Player evasion/damage reduct
+        rand = GetRandomNumber(100);
+        if (rand > (player.chanceOfEvasion * 100))
+        {
+            rand = GetRandomNumber(100);
+            if (rand > (player.chanceOfDamageReduction * 100))
+            {
+                cout << "You take " << damage << " damage" << endl;
+                player.currentHp -= damage;
+            }
+            else
+            {
+                if (damage > player.damageReduction)
+                {
+                    cout << "You took reduced damage" << endl;
+                    player.currentHp -= damage - player.damageReduction;
+                }
+                else
+                {
+                    cout << "You negated the enemy's attack with your damage reduction" << endl;
+                }
+            }
+
+        }
+        else
+        {
+            cout << "You evaded the enemy's attack" << endl;
+        }
+    }
+
+}
+
 void OutTown(Character &player)
 {
     player.position = 0;
     int opt;
     bool exit = false;
-    while(player.IsAlive())
+    do
     {
         Options(player.position);
         cin >> opt;
-        Calculate(player, opt, exit);
+        MoveCharacter(player, opt, exit);
         if (exit) break;
+        if (CheckIfMeetEnemy(player))
+        {
+            cout << "LOG: ENEMY DETECTED!" << endl;
+            
+            Monster monster;
+            SetEnemyStats(monster, player);
+            ShowEnemyStats(monster);
+            
+            SetPlayerStats(player, monster);
 
-    }
+            //Early Attack for Archers
+            for (int i = 0; i < player.amountOfExtraActionAtStartTurn; i++)
+            {
+                cout << "You can attack " << player.amountOfExtraActionAtStartTurn - i << " turns more." << endl;
+                ShowBattleOptions();
+                cin >> opt;
+                CalculatePlayerAttack(player, monster, opt);
+                ShowEnemyStats(monster);
+            }
+
+            //Fighting
+            do
+            {
+                ShowBattleOptions();
+                cin >> opt;
+                CalculatePlayerAttack(player, monster, opt);
+                ShowEnemyStats(monster);
+                if (!monster.IsAlive()) break;
+
+                cout << "Monster is attacking  .." << endl;
+                CalculateMonsterAttack(monster, player, opt);
+                ShowPlayerStats(player);
+            }while(monster.IsAlive() && player.IsAlive());
+            if (!monster.IsAlive()) 
+            {
+                cout << "You killed the monster!"           << endl
+                     << "You get " << monster.gold << " G." << endl;
+                player.gold += monster.gold;
+            }else cout << "You have been defeated!"         << endl;
+        }
+    }while(player.IsAlive());
 }
-
--- CyroRPG/OutTown.h
+++ CyroRPG/OutTown.h
@@ -1,10 +1,24 @@
 #pragma once
 #include <iostream>
 #include "./Characters.h"
+#include "./Monsters.h"
+#include "./Random.h"
 #include "./Misc.h"
 
 void Options();
 
-void Calculate(Character &player, int opt, bool &exitStatus);
+void MoveCharacter(Character &player, int opt, bool &exitStatus);
+
+bool CheckIfMeetEnemy(Character &player);
+
+void SetPlayerStats(Character &player, Monster &monster);
+
+void SetEnemyStats(Monster &monster, Character &player);
+
+void ShowBattleOptions();
+
+void CalculatePlayerAttack(Character &player, Monster &monster, int opt);
+
+void CalculateMonsterAttack(Monster &monster, Character &player, int opt);
 
 void OutTown(Character &player);
