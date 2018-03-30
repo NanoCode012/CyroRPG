68f2e8ae0e3bde13affbab1c35127813ca539b18
NanoCode012 <kevinvong@rocketmail.com>
2018-03-30 17:27:29 +0700

    Added Item Shop, Show Item stats, Changed layout of player stats output Added defense

--- CyroRPG/InTown.cpp
+++ CyroRPG/InTown.cpp
@@ -68,30 +68,170 @@ void Inn(Character &player, bool isDead)
     }
 }
 
+void GenerateItem(Item *items, int index, int playerLevel)
+{
+    Item item;
+    int rarity = GetRandomNumber(playerLevel / 10);
+    if (rarity < 0) rarity = 0;
+    else if (rarity > 5) rarity = 5;
+    item.SetType(GetRandomNumber(3), rarity, (bool)GetRandomNumber(1));
+    items[index] = item;
+}
+
+void ShowItemStats(Item item, bool showName)
+{
+    if (showName)
+    {
+        cout << "\tName : " << item.name << endl;
+    }
+    cout  << "\tCost : "      << item.cost << "G"                             << endl
+          << "\tCan Equip : " << ((item.isEquippable) ? "Yes" : "No")         << endl
+          << "\tRarity : "    << item.level                                   << endl;
+    if (item.isEquippable)
+    {
+        switch(item.type)
+        {
+            case 0:
+                cout << "\tDamage Reduction : " << item.damageReduction << endl;
+                break;
+            case 1:
+                cout << "\tDefense : " << item.defense << endl;
+                break;
+            case 2:
+                cout << "\tCritical Multiplier : " << item.criticalDamagePercentage << endl;
+                break;
+            case 3:
+                cout << "\tEvasion : " << item.chanceOfEvasion << endl;
+                break;
+        }
+    }
+    else
+    {
+        switch(item.type)
+        {
+            case 0:
+                cout << "\tHP : " << item.hp << endl;
+                break;
+            case 1:
+                cout << "\tMana : " << item.mana << endl;
+                break;
+            case 2:
+                cout << "\tEvasion : " << item.chanceOfEvasion << endl;
+                break;
+            case 3:
+                cout << "\tCritical Chance : " << item.chanceOfCriticalDamage << endl;
+                break;
+        }
+    }
+}
+
+void ItemShop(Character &player)
+{
+    PrintLine();
+
+    int amountOfItemsInShop = 3;
+    Item *items = new Item[amountOfItemsInShop];
+    bool *itemsBought = new bool[amountOfItemsInShop];
+
+    for (int i = 0; i < amountOfItemsInShop; i++)
+    {
+        GenerateItem(items, i, player.level);
+    }
+
+    cout << "Welcome to my lovely shop" << endl;
+    while(true)
+    {
+        cout << "What would you like to purchase?" << endl;
+        for (int i = 0; i < amountOfItemsInShop; i++)
+        {
+            if (!itemsBought[i])
+            {
+                cout << i + 1 << ". " << items[i].name        << endl;
+                ShowItemStats(items[i], false);
+            }
+        }
+        cout << amountOfItemsInShop + 1 << ". Exit" << endl;
+        
+        int opt;
+        cout << "Opt : ";
+        cin >> opt;
+
+        if (opt == amountOfItemsInShop + 1) break;
+        else if (opt > amountOfItemsInShop) 
+        {
+            cout << "Invalid Option" << endl;
+        }
+        else if (itemsBought[opt - 1])
+        {
+            cout << "Item already bought" << endl;
+        }
+        else
+        {
+            if (player.amountOfItems < player.maxAmountOfItems)
+            {
+                if (player.gold >= items[opt - 1].cost)
+                {
+                    cout << "Thank you for your patronage" << endl;
+                    player.gold -= items[opt - 1].cost;
+                    player.Insert(items[opt - 1]);
+
+                    itemsBought[opt - 1] = true;
+                }
+                else
+                {
+                    cout << "You do not have enough G" << endl;
+                }
+            }
+            else
+            {
+                cout << "You do not have enough space" << endl;
+            }
+        }
+
+    }
+}
+
 void ShowPlayerStatsFull(Character &player)
 {
     PrintLine();
     
-    cout << "Player Stats\n"
-         << "\nName : " << player.name
-         << "\nHP : " << player.currentHp << " / " << player.maxHp
-         << "\nMP : " << player.currentMana << " / " << player.maxMana
-         << "\nLevel : " << player.level
-         << "\nExp : " << player.currentExp << " / " << player.expToNextLevel
-         << "\nG : " << player.gold
-         << "\nClass : " << player.GetClassName()
-         << "\nAttack Power : " << player.attackDamageMin << " - " << player.attackDamageMax
-         << "\nDefense : " << player.defense
-         << "\nEvasion : " << player.chanceOfEvasion * 100 << " % "
-         << "\nDamage Reduction Chance : " << player.chanceOfDamageReduction * 100 << " % "
-         << "\nDamage Reduction : " << player.damageReduction
-         << "\nCritical Chance : " << player.chanceOfCriticalDamage * 100 << " % "
+    cout << "\nStats\n"
+         << "\n\tName : " << player.name
+         << "\n\tHP : " << player.currentHp << " / " << player.maxHp
+         << "\n\tMP : " << player.currentMana << " / " << player.maxMana
+         << "\n\tLevel : " << player.level
+         << "\n\tExp : " << player.currentExp << " / " << player.expToNextLevel
+         << "\n\tG : " << player.gold
+         << "\n\tClass : " << player.GetClassName()
+         << "\n\tAttack Power : " << player.attackDamageMin << " - " << player.attackDamageMax
+         << "\n\tDefense : " << player.defense
+         << "\n\tEvasion : " << player.chanceOfEvasion * 100 << " % "
+         << "\n\tDamage Reduction Chance : " << player.chanceOfDamageReduction * 100 << " % "
+         << "\n\tDamage Reduction : " << player.damageReduction
+         << "\n\tCritical Chance : " << player.chanceOfCriticalDamage * 100 << " % "
          << endl;
 
-    cout << "\nPlayer Skills\n" << endl;
+    cout << "\nSkills\n" << endl;
     for (int i = 0; i < player.amountOfSkills; i++)
     {
-        cout << player.skills[i].name << "(Lvl: " << player.skills[i].level << ")" << endl;
+        cout << "\t" << player.skills[i].name << "(Lvl: " << player.skills[i].level << ")" << endl;
+    }
+
+    cout << "\nEquipped Gear\n" << endl;
+    for (int i = 0; i < 5; i++)
+    {
+        if (!player.equipped[i].isNull)
+        {
+            ShowItemStats(player.equipped[i]);
+            cout << endl;
+        }
+    }
+
+    cout << "\nInventory\n" << endl;
+    for (int i = 0; i < player.amountOfItems; i++)
+    {
+        ShowItemStats(player.inventory[i]);
+        cout << endl;
     }
 
     PrintLine();
@@ -272,6 +412,7 @@ void InTown(Character &player)
     {
         OptionsInTown();
         cin >> opt;
+        if (opt > 10) continue;
         if (opt == 5) break;
         switch(opt)
         {
@@ -282,7 +423,7 @@ void InTown(Character &player)
                 //MissionBoard(player);
                 break;
             case 3:
-                //ItemShop(player);
+                ItemShop(player);
                 break;
             case 4:
                 ShowPlayerStatsFull(player);
--- CyroRPG/InTown.h
+++ CyroRPG/InTown.h
@@ -3,11 +3,16 @@
 #include <stdlib.h>
 #include <fstream>
 #include "./Characters.h"
+#include "./Items.h"
 #include "./Random.h"
 #include "./Misc.h"
 
 void OptionsInTown();
 void Inn(Character &player, bool isDead = false);
+
+void GenerateItem(Item *items, int index, int playerLevel);
+void ShowItemStats(Item item, bool showName = true);
+void ItemShop(Character &player);
 
 void ShowPlayerStatsFull(Character &player);
 
